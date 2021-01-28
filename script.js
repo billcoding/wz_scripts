ec90c5054fb9090651baefc956c278720a5a1768
billcoding <wangmiao07@sina.com>
2021-01-29 00:23:40 +0800

    Update

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function 物品类型(itemId) {
     return itemType(itemId)
 }
 
+function 抽奖物品属性(type) {
+    return lotteryItemAttr(type);
+}
+
 function randRange(a, b) {
     if (!(typeof (a) === "number" && typeof (b) === "number")) {
         return 0;
@@ -24,4 +28,25 @@ function randRange(a, b) {
 
 function itemType(itemId) {
     return parseInt((itemId + "")[0])
+}
+
+function lotteryItemAttr(type) {
+    var maxI = 10;
+    var maxJ = 6;
+    var double = 10;
+    if (type < 0) {
+        type = 0;
+    }
+    if (type > maxI) {
+        type = 10;
+    }
+    var attrs = [];
+    for (var i = 0; i < maxI; i++) {
+        var a = [];
+        for (var j = 0; j < maxJ; j++) {
+            a.push((i + 1) * double);
+        }
+        attrs.push(a);
+    }
+    return attrs[type - 1];
 }
